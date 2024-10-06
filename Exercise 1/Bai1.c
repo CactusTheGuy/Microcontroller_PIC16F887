b3e3009bd26d8e356fb67ddb50ed58e21f9a963f
CactusTheGuy <75614340+CactusTheGuy@users.noreply.github.com>
2024-10-06 07:35:01 +0700

    update to match the submitted version
    forgot that if want to change the port from input to output and vice versa, you read from right to left from 0 to 7

--- Exercise 1/Bai1.c
+++ Exercise 1/Bai1.c
@@ -1,10 +1,10 @@
 #include <Bai1.h>
 unsigned int8 byte_c = 0;  //Store the number of time LED blink
-unsigned ini1 start_hold = 0;  //Make LED blinking even if START is released after press
+unsigned int1 start_hold = 0;  //Make LED blinking even if START is released after press
 
 void main()
 {
-   set_tris_b(0x7f);  //Setup port B at B0 to output, B1 to B7 to input
+   set_tris_b(0xfe);  //Setup port B at B0 to output, B1 to B7 to input
    set_tris_c(0x00);  //Setup port C to all output
    set_tris_d(0xff);  //Setup port D to all input
    output_low(PIN_B0);  //Give the output pin a low signal to ensure a correct output of the pin
@@ -17,7 +17,7 @@ void main()
         if (input(PIN_D0))  //Check if SET button is released
         {
             byte_c++;
-            output_c(byte_c);
+            output_c(byte_c);  //update after adding
         }
       }  //This function is used to give the SET button to register only 1 time
       if (!input(PIN_D1))  //Check if the START button is pressed
@@ -25,10 +25,10 @@ void main()
          delay_ms(10);
          if (input(PIN_D1) & (start_hold==0) & (byte_c>0))
          {
-             start_byte=1;
+             start_hold=1;
          }
       }  //This function is used to give the START button to register only 1 time
-      if (start_hold==1 & (byte_c>0))  //Check if the varibles is a possitive number
+      if (start_hold==1 & (byte_c>0))  //Check if the varibles is a positive number
       {
          byte_c--;
          output_c(byte_c); //Update after subtracting
