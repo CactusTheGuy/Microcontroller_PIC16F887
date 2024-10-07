c8da6cd956cfa7b14800d74030b1957c02b8ef15
CactusTheGuy <75614340+CactusTheGuy@users.noreply.github.com>
2024-10-07 19:12:39 +0700

    Add files via upload

--- Exercise 4/Bai4.c
+++ Exercise 4/Bai4.c
@@ -2,8 +2,8 @@
 #define RS PIN_E0
 #define RW PIN_E1
 #define E PIN_E2
-unsigned int16 temp;
-float StepSize = 4.887;
+unsigned int16 Temp, VariRes;
+float StepSize = 4.887, VR_Value, T_Value;
 
 void lcd_command(unsigned int8 cm)
 {
@@ -39,20 +39,32 @@ void main()
    set_tris_c(0x00);
    set_tris_d(0x00);
    set_tris_e(0x00);
-   setup_adc(ADC_CLOCK_DIV_8);
-   setup_adc_ports(sAN0);
-   set_adc_channel(0);
    lcd_setup();
+   setup_adc(ADC_CLOCK_DIV_8);
+   setup_adc_ports(sAN0|sAN1|VSS_VDD);
    while(TRUE)
    {
-      temp=(StepSize*read_adc())/10;
+      set_adc_channel(0);
+      delay_ms(10);
+      T_Value=read_adc();
+      Temp=(StepSize*T_Value)/10;
       lcd_command(0x80);
       lcd_data("Nhiet Do: ");
-      lcd_data(temp/10+ 0x30);
-      lcd_data(temp%10+ 0x30);
+      lcd_data((temp/10)+ 0x30);
+      lcd_data((temp%10)+ 0x30);
       lcd_data(0xDF);
       lcd_data("C");
-      if (temp>29)
+      set_adc_channel(1);
+      delay_ms(10);
+      VR_Value=read_adc();
+      VariRes=(100*VR_Value)/1023;
+      lcd_command(0xc0);
+      lcd_data("Nhiet Do Dat: ");
+      lcd_data((VariRes/10)+ 0x30);
+      lcd_data((VariRes%10)+ 0x30);
+      lcd_data(0xDF);
+      lcd_data("C");
+      if (temp>VariRes)
       {
          output_high(PIN_C0);
       }
