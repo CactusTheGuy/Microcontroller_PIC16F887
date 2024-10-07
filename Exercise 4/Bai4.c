08f14d1bbc9f824fc970056666193290b24dc9bc
CactusTheGuy <75614340+CactusTheGuy@users.noreply.github.com>
2024-10-08 05:39:23 +0700

    Add files via upload

--- Exercise 4/Bai4.c
+++ Exercise 4/Bai4.c
@@ -3,9 +3,10 @@
 #define RW PIN_E1
 #define E PIN_E2
 unsigned int16 Temp, VariRes;
-float StepSize = 4.887, VR_Value, T_Value;
+float VR_Value, T_Value;
+float StepSize = 4.887;
 
-void lcd_command(unsigned int8 cm)
+void lcd_command(unsigned int8 cm)  //This function is used to command where the characters is displaying to LCD display
 {
    output_low(RW);
    output_low(RS);
@@ -14,7 +15,7 @@ void lcd_command(unsigned int8 cm)
    output_high(E);
    delay_ms(1); 
 }
-void lcd_data(unsigned int8 dt)
+void lcd_data(unsigned int8 dt)  //This function is used to display a set ASICII characters to LCD display
 {
    output_low(RW);
    output_high(RS);
@@ -23,7 +24,7 @@ void lcd_data(unsigned int8 dt)
    output_high(E);
    delay_ms(1);   
 }
-void lcd_setup()
+void lcd_setup()  //This function is used to set up the LCD display when powered on
 {
    delay_ms(5);
    lcd_command(0x38);
@@ -33,42 +34,42 @@ void lcd_setup()
    lcd_command(0x01);
    delay_ms(2);
 }
-void main()
+void main()  //Main program start
 {
-   set_tris_a(0xff);
-   set_tris_c(0x00);
-   set_tris_d(0x00);
-   set_tris_e(0x00);
-   lcd_setup();
-   setup_adc(ADC_CLOCK_DIV_8);
-   setup_adc_ports(sAN0|sAN1|VSS_VDD);
+   set_tris_a(0xff);  //Set port A to all input
+   set_tris_c(0x00);  //Set port C to all output
+   set_tris_d(0x00);  //Set port D to all output
+   set_tris_e(0x00);  //Set port E to all output
+   lcd_setup();  //Run the function command
+   setup_adc(ADC_CLOCK_DIV_8);  //Set up the ADC so the ADC is run at processor speed divide by 8
+   setup_adc_ports(sAN0|sAN1|VSS_VDD);  //Set up what port of ADC is used
    while(TRUE)
    {
-      set_adc_channel(0);
-      delay_ms(10);
-      T_Value=read_adc();
-      Temp=(StepSize*T_Value)/10;
-      lcd_command(0x80);
-      lcd_data("Nhiet Do: ");
+      set_adc_channel(0);  //Switch to channel 0 of ADC
+      delay_ms(1);  //Add delay to switch the channel
+      T_Value=read_adc();  //Read the value from ADC and import to a variable
+      Temp=(StepSize*T_Value)/10;  //Calculate the tempeture using formula
+      lcd_command(0x80); //Run the function command (Characters start at first line)
+      lcd_data("Nhiet Do: ");  //Display the ASICII with specified word
       lcd_data((temp/10)+ 0x30);
       lcd_data((temp%10)+ 0x30);
-      lcd_data(0xDF);
+      lcd_data(0xDF);  //Typing out the int8 data due to unable to type out the character
       lcd_data("C");
-      set_adc_channel(1);
-      delay_ms(10);
-      VR_Value=read_adc();
-      VariRes=(100*VR_Value)/1023;
-      lcd_command(0xc0);
-      lcd_data("Nhiet Do Dat: ");
+      set_adc_channel(1);  //Switch to channel 1 of ADC
+      delay_ms(1);  //Add delay to switch the channel
+      VR_Value=read_adc(); //Read the value from ADC and import to another variable
+      VariRes=(100*VR_Value)/1023;  //Calculate the variable resistor using formula to output the set tempeture
+      lcd_command(0xc0);  //Run the function command (Characters start at second line)
+      lcd_data("Nhiet Do Dat: ");  //Display the ASICII with specified word
       lcd_data((VariRes/10)+ 0x30);
       lcd_data((VariRes%10)+ 0x30);
-      lcd_data(0xDF);
+      lcd_data(0xDF);  //Typing out the int8 data due to unable to type out the character
       lcd_data("C");
-      if (temp>VariRes)
+      if (temp>VariRes)  //Check if the calculated tempeture value is higher than the calculated value of set tempeture
       {
          output_high(PIN_C0);
       }
-      else
+      else  //If condition is not meet, run from this line instead
       {
          output_low(PIN_C0);
       }
